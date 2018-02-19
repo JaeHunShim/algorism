0d93bcdadc73b9dc4c85abbd57daf9e8fb3b0bfa
LAPTOP-J0CSRS4B\jaehun <jaehuniya@gmail.com>
2018-02-19 22:25:12 +0900

    정수를 오름차순으로 정렬하기

--- src/algorism/HarshadNumber.java
+++ src/algorism/HarshadNumber.java
@@ -24,7 +24,7 @@ public class HarshadNumber {
   
        // 아래는 테스트로 출력해 보기 위한 코드입니다.
 	public static void  main(String[] args){
-		ex sn = new ex();
+		HarshadNumber sn = new HarshadNumber();
 		System.out.println(sn.isHarshad(18));
 	}
 }
