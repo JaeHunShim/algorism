bb95bd8db288f0d12df563112e458cda4df887c1
LAPTOP-J0CSRS4B\jaehun <jaehuniya@gmail.com>
2018-03-03 18:42:35 +0900

    최고의 집합

---
+++ src/algorism/Midium/Hopscotch.java
@@ -0,0 +1,17 @@
+package algorism.Midium;
+
+public class Hopscotch {
+
+	public static void main(String[] args) {
+		
+		int[][] board  = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+		int size=3;
+		
+		int result= 0;
+		
+		for(int i=0; i<board.length; i++) {
+			
+		}
+	}
+	
+}
